42d034242b6a6af7108fe659542d152c50b342f7
jueti <18200748882@163.com>
2017-12-05 01:22:37 +0800

    complete!

--- 001/sha1.py
+++ 001/sha1.py
@@ -2,34 +2,36 @@ import hashlib
 import os
 import sys
 
-def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
 
-def sha1(file_name):
-    sha1hash = hashlib.sha1()
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def get_sha1(file_name):
+    sha1 = hashlib.sha1()
     file = open(file_name, 'rb')
     while True:
         buffer = file.read(8096)
         if not buffer:
             break
-        sha1hash.update(buffer)
+        sha1.update(buffer)
     file.close()
-    return(sha1hash.hexdigest())
+    return sha1.hexdigest()
+
 
 def get_file_name():
-    if len(sys.argv)!=2:
+    if len(sys.argv) != 2:
         filename = input("Please input the file URL:")
         return filename
     else:
         return sys.argv[1]
 
+
 if __name__ == "__main__":
-    file_name = get_file_name()
+    file_url = get_file_name()
     print("计算中")
-    sha1hash = sha1(file_name)
+    sha1_hash = get_sha1(file_url)
     cls()
-    print(sha1hash)
+    print(sha1_hash)
     print("\n")
     input("Press <enter>")
-
-
