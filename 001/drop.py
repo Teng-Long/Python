52b3c27083a53575b464c0226d60ab749e51ee70
jueti <18200748882@163.com>
2017-12-05 01:06:34 +0800

    complete!

---
+++ 001/drop.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def get_file_name():
+    if len(sys.argv) <= 1:
+        file_name = input("Please input the file URL:")
+        return file_name
+    else:
+        return sys.argv[1]
+
+
+if __name__ == "__main__":
+    distinct_url = get_file_name()
+    print("        type: ", type(sys.argv))
+    print("         len: ", len(sys.argv))
+    print("         str: ", str(sys.argv))
+    print(" Current_URL: ", sys.argv[0])
+    print("Distinct_URL: ", distinct_url)
+    input("Press <enter>")
