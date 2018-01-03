b767039c5302d9cafd5a2186b3c11b4a1fb86492
jueti <18200748882@163.com>
2018-01-03 20:44:03 +0800

    中文转 url 编码

--- 002/chinese_to_url_v0.1.0.py
+++ 002/chinese_to_url_v0.1.0.py
@@ -5,7 +5,7 @@
     版本：0.1.0
     日期：2018-1-3
     许可证：GPL3+
-    0.1.0 新增功能：（1）将中文转码为 url（2）将 url 转为中文
+    0.1.0 新增功能：（1）将中文转码为 url（2）将 url 解码
 """
 from urllib.parse import quote
 from urllib.parse import unquote
@@ -14,7 +14,7 @@ from urllib.parse import unquote
 def print_main_menu():
     print(' Main Menu '.center(50, '*'))
     print('    1: 将中文转码为 url')
-    print('    2: 将 url 转码为中文')
+    print('    2: 将 url 解码')
     print('    0: 退出')
 
 
@@ -26,11 +26,11 @@ def chinese_to_url():
     print('程序返回！')
 
 
-def url_to_chinese():
+def url_unquote():
     print(' Url to Chinese '.center(50, '*'))
-    string_chinese = input('Please input: ')
-    string_url = unquote(string_chinese)
-    print('转码结果：{}'.format(string_url))
+    string_url = input('Please input: ')
+    string_unquote = unquote(string_url)
+    print('转码结果：{}'.format(string_unquote))
     print('程序返回！')
 
 
@@ -41,6 +41,6 @@ if __name__ == '__main__':
         if choice == '1':
             chinese_to_url()
         elif choice == '2':
-            url_to_chinese()
+            url_unquote()
         elif choice == '0':
             break
